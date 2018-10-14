9ee75cc98c7857fe4e9f201aa34e8a159598c575
Rafael de Souza Freire <rafaels.freire999@gmail.com>
2018-10-14 20:06:25 -0200

    Implementação - Relatório de Histórico de Preços

--- src/main/java/br/com/sigem/controller/HistoricoPrecoController.java
+++ src/main/java/br/com/sigem/controller/HistoricoPrecoController.java
@@ -1,10 +1,14 @@
 package br.com.sigem.controller;
 
 
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.sigem.security.Conexao;
 import br.com.sigem.service.HistoricoPrecoService;
+import br.com.sigem.util.GeradorRelatorio;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
 @Controller
 @RequestMapping("/sigem/relatorios/historicoPreco")
@@ -48,6 +55,34 @@ public class HistoricoPrecoController {
 			return mv;
 		}
 		
+	}
+	
+	@PostMapping("/gerarPdf")
+	public ModelAndView gerarPdf(@RequestParam("dataInicioModal") String dataInicio, @RequestParam("dataFimModal") String dataFim) {
+		
+		String nomeArquivo = "historico_preco.jasper";
+		String caminho = new File("./").getAbsolutePath(); 
+		caminho = caminho.substring(0, caminho.length() - 1);
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date data_inicio, data_final;
+		
+		try {
+			data_inicio = sdf.parse(dataInicio);
+			data_final = sdf.parse(dataFim);
+			params.put("DATA_INICIO", data_inicio);
+			params.put("DATA_FIM", data_final);
+			GeradorRelatorio geradorRelatorio = new GeradorRelatorio(nomeArquivo, params, Conexao.getConnection());
+			geradorRelatorio.gerarPDFParaOutputStream(new SimpleOutputStreamExporterOutput(caminho+"histórico_de_preços.pdf"));
+			
+			return new ModelAndView("redirect:/sigem/relatorios/historicoPreco").addObject("gerado", true);
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/sigem/relatorios/historicoPreco").addObject("error", true);
 	}
 
 	
