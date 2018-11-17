4ac13f34e60f2a7b4cd603a21a09696c053bac61
Alan Lopes <alanlopes4@gmail.com>
2018-11-17 18:21:20 -0200

    Implementação - Estoque e Gôndola - aviso caso o estoque esteja menor do que o estoque mínimo

--- src/main/java/br/com/sigem/service/EstoqueService.java
+++ src/main/java/br/com/sigem/service/EstoqueService.java
@@ -44,12 +44,73 @@ public class EstoqueService {
 	
 	
 	public Estoque atualizar(@Valid Estoque estoque) {
-			
-			estoque.setEstoque(estoque.getEstoque() - estoque.getAdicionadaGondola());
-			estoque.setGondola(estoque.getGondola() + estoque.getAdicionadaGondola());
+	
+		int quantidade_estoque = atualizarEstoque(estoque.getEstoque(), estoque.getAdicionadaGondola(), estoque.getEstoqueMinimo());
+		int quantidade_gondola = atualizarGondola(estoque.getGondola(), estoque.getAdicionadaGondola());
+			if(quantidade_estoque == -1)
+				estoque.setMensagem("Quantidade a retirar maior do que a quantidade disponível no estoque");
+			else if(quantidade_estoque == -2)
+				estoque.setMensagem("O estoque está vazio");
+			else if(quantidade_estoque == -3)
+				estoque.setMensagem("Quantidade a se retirar inválida");
+			else if(quantidade_estoque == -4)
+				estoque.setMensagem("Quantidade no estoque inválida");
+			else if(quantidade_estoque == -5)
+				estoque.setMensagem("Quantidade minima no estoque inválida");
+			else if(quantidade_estoque < estoque.getEstoqueMinimo()) {
+				estoque.setMensagem("É necessário abastecer o estoque");
+				estoque.setEstoque(quantidade_estoque);
+			}
+			else {
+				estoque.setMensagem("");
+				estoque.setEstoque(quantidade_estoque);
+			}
+				
+		
+		
+			if(quantidade_gondola == -1)
+				estoque.setMensagem("Quantidade na gôndola inválida");
+			else if(quantidade_gondola == -2)
+				estoque.setMensagem("Quantidade a adicionar inválida");
+			else {
+		    if(estoque.getMensagem().equals("")) estoque.setMensagem("");
+			estoque.setGondola(quantidade_gondola);
+			}
 		
 	        return estoqueRepository.saveAndFlush(estoque);
-	   }
+	 }
+	
+	private int atualizarEstoque(int quantidade_atual_estoque, int quantidade_retirada, int quantidade_minima) {
+		
+		int resultado = 0;
+		if(quantidade_retirada > quantidade_atual_estoque)
+			resultado = -1;
+		else if (quantidade_atual_estoque == 0)
+			resultado = -2;
+		else if(quantidade_retirada < 0)
+			resultado = -3;
+		else if(quantidade_atual_estoque < 0)
+			resultado = -4;
+		else if(quantidade_minima < 0)
+			resultado = -5;
+		else
+			resultado =  quantidade_atual_estoque - quantidade_retirada;
+		
+		return resultado;
+		
+	}
+	
+	private int atualizarGondola(int quantidade_atual_gondola, int quantidade_adicionada) {
+		int resultado = 0;
+			if(quantidade_atual_gondola < 0)
+				resultado = -1;
+			else if(quantidade_adicionada < 0)
+				resultado = -2;
+			else
+				resultado = quantidade_atual_gondola + quantidade_adicionada;
+		return resultado;
+		
+	}
 	
 	public Estoque retirar(@Valid Estoque estoque) {
 		
